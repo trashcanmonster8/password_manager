4c7c17f588acd62ce70df6edb37ad9921b9b4ca0
Jake <jacob.michael.mayer@gmail.com>
2019-02-26 22:45:19 -0500

    initial commit

---
+++ lib/password_manager.rb
@@ -0,0 +1,6 @@
+require "password_manager/version"
+
+module PasswordManager
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/password_manager/version.rb
@@ -0,0 +1,3 @@
+module PasswordManager
+  VERSION = "0.1.0"
+end
