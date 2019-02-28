cf5f9a93828e2195fad1593fbc492d6a1a1af36b
Jake <jacob.michael.mayer@gmail.com>
2019-02-27 22:11:38 -0500

    Creates new password using faker gem

--- password_manager.gemspec
+++ password_manager.gemspec
@@ -35,4 +35,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 2.0'
   spec.add_development_dependency 'rake', '~> 12.3'
   spec.add_development_dependency 'rspec', '~> 3.8'
+  spec.add_runtime_dependency 'faker'
 end
--- spec/credentials_spec.rb
+++ spec/credentials_spec.rb
@@ -2,4 +2,11 @@ require 'password_manager/credentials'
 
 RSpec.describe PasswordManager::Credentials do
   it { is_expected.to have_attributes username: nil, password: nil }
+
+  it 'creates new passwords' do
+    creds = described_class.new
+    old_password = creds.password
+    creds.update
+    expect(creds.password).not_to eq old_password
+  end
 end
