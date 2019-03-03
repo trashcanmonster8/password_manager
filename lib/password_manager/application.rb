ae907958d125b7339652ae3ce28a3ccf2f5f2983
Jake <jacob.michael.mayer@gmail.com>
2019-03-02 21:09:35 -0500

    Build initial application file

--- lib/password_manager/application.rb
+++ lib/password_manager/application.rb
@@ -3,28 +3,42 @@ require 'password_manager/password'
 module PasswordManager
   class Application
     FILE = '~/.creds'.freeze
+    DEFAULT_CREDS = Password::DEFAULT_OPTS.dup.merge(
+      username: nil,
+      password: nil
+    )
 
     def self.run
-      @@creds = YAML.safe_load File.read(FILE)
+      file = File.open(FILE, 'w')
+      @@creds = YAML.safe_load(file.read)
       application
-      File.write(FILE, 'w') { |f| f.write @@creds.to_yaml }.close
+      f.write(@@creds.to_yaml).close
     end
 
     def self.application
-      @@creds.each do |account, password|
-        puts "Update password for #{account}"
-        input = gets.chomps
-        password = update?(input, password)
+      puts 'What account do you want to update?'
+      account = gets.chomp.to_sym
+      cred = @@creds.fetch(account, DEFAULT_CREDS)
+      puts "Do you want to update the creds for #{account}"
+      puts "Current creds\n   username: #{cred[:username]}\n   password: #{cred[:password]}\n"
+      unless cred[:username]
+        puts 'Type username'
+        cred[:username] = gets.chomp
       end
+      puts "Update password for #{account}"
+      input = gets.chomps
+      password = update?(input, password)
+      @@creds[account] = cred
     end
 
     def self.update?(input, password)
+      puts "Old password is: #{password}"
       return password if input.casecmp('n').zero?
 
       new_password = Password.new(password)
       new_password.Update
       puts "New password is: #{password}"
       new_password.password
-      end
+    end
   end
-end
+ends
