6ce0237f4e9eb0df2449ef18e50aa53dec01dcf2
Jake <jacob.michael.mayer@gmail.com>
2019-03-02 21:47:07 -0500

    Update application.rb

--- lib/password_manager/application.rb
+++ lib/password_manager/application.rb
@@ -9,36 +9,39 @@ module PasswordManager
     )
 
     def self.run
-      file = File.open(FILE, 'w')
-      @@creds = YAML.safe_load(file.read)
+      file = File.open(File.expand_path(FILE), 'w+')
+      @@creds = @@safe = YAML.safe_load(file.read) || {}
       application
-      f.write(@@creds.to_yaml).close
+      file.write(@@creds.to_yaml).close
+    rescue
+      file.write(@@safe.to_yaml).close
     end
 
     def self.application
       puts 'What account do you want to update?'
-      account = gets.chomp.to_sym
-      cred = @@creds.fetch(account, DEFAULT_CREDS)
+      account = gets.chomp
+      cred = @@creds[account] || {}
       puts "Do you want to update the creds for #{account}"
       puts "Current creds\n   username: #{cred[:username]}\n   password: #{cred[:password]}\n"
       unless cred[:username]
         puts 'Type username'
         cred[:username] = gets.chomp
       end
-      puts "Update password for #{account}"
-      input = gets.chomps
-      password = update?(input, password)
+      puts "Update password ([nN] = no update, [rR] = new random, or type new password) #{account}"
+      input = gets.chomp
+      cred[:password] = update?(input, cred[:password])
+      puts "New password is: #{cred[:password]}"
       @@creds[account] = cred
     end
 
     def self.update?(input, password)
-      puts "Old password is: #{password}"
-      return password if input.casecmp('n').zero?
+      return password if input.downcase == 'n'
+
+      return input unless input.downcase == 'r'
 
       new_password = Password.new(password)
-      new_password.Update
-      puts "New password is: #{password}"
+      new_password.update
       new_password.password
     end
   end
-ends
+end
