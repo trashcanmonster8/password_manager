b233a369ae98d67219e8010c7fb73e9845205b97
Jake <jacob.michael.mayer@gmail.com>
2019-03-26 22:28:20 -0400

    Create thor based application

--- spec/application_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'password_manager/application'
-
-RSpec.describe PasswordManager::Application do
-  it 'prints version' do
-    expect { subject.version }.to output("password_manager v#{PasswordManager::VERSION}\n").to_stdout
-  end
-end
