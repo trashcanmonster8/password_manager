dc35aa6bfa6e908d3e647a49067352f74685dd6b
Jake <jacob.michael.mayer@gmail.com>
2019-04-15 13:00:09 -0400

    created set configuration method

--- lib/password_manager/application.rb
+++ lib/password_manager/application.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
-require 'password_manager/version'
+require 'password_manager/constants'
 require 'thor'
 
 module PasswordManager
   class Application < Thor
+    include Constants
+
     desc 'version', 'Display current version'
     def version
-      say "v#{PasswordManager::VERSION}"
+      say "v#{VERSION}"
+    end
+
+    desc 'password', 'Set password'
+    def password(password)
+      File.open(CONFIGURATION, File::CREAT).write(
+        {
+          salt: SecureRandom.base64,
+          password: Base64.strict_encode64(password)
+        }.to_yaml
+      )
     end
   end
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,11 +1,33 @@
 # frozen_string_literal: true
 
 require 'password_manager/application'
+require 'password_manager/constants'
+
+TEST_COFIG = <<~HEREDOC
+  salt: "VgJyUZY3/Eu7u1gO0u4QIw==\n"
+  password: dGVzdA==
+HEREDOC
 
 RSpec.describe PasswordManager::Application do
   subject { PasswordManager::Application.new }
 
   specify 'prints version' do
-    expect { subject.version }.to output("v#{PasswordManager::VERSION}\n").to_stdout
+    expect { subject.version }.to output("v#{PasswordManager::VERSION}\n")
+      .to_stdout
+  end
+
+  context '.password_manager' do
+    it 'set password' do
+      file_dbl = instance_double(File)
+      allow(SecureRandom).to receive(:base64)
+      .and_return('h+VrEp/1tSIbEnXtXQ1fcQ==')
+      allow(File).to receive(:open)
+        .with(PasswordManager::Constants::CONFIGURATION, File::CREAT)
+        .and_return(file_dbl)
+      allow(file_dbl).to receive(:close)
+      expect(file_dbl).to receive(:write)
+        .with("---\n:salt: h+VrEp/1tSIbEnXtXQ1fcQ==\n:password: dGVzdA==\n")
+      subject.password('test')
+    end
   end
 end
